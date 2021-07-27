9a73dfabf61396f229e0bf5e652527631b060f5e
jsenlas <you@example.com>
2021-07-27 19:07:58 +0200

    all done

--- Firmware/Arduino/LowLatencyLoggerMPU6050_ADS1115/LowLatencyLoggerMPU6050_ADS1115.ino
+++ Firmware/Arduino/LowLatencyLoggerMPU6050_ADS1115/LowLatencyLoggerMPU6050_ADS1115.ino
@@ -1,5 +1,5 @@
 // Empty file with name LowLatencyLoggerMPU6050.ino to make IDE happy.
 
-// This project is heavily based on SDFat.h library example. We just added ADS1115 functionality.
+// This project is heavily based on SDFat.h library example. We added ADS1115 functionality.
 // Author: Jakub Sencak
-// There were no files recorded using this code...
+// Date:   2021/10/2020
